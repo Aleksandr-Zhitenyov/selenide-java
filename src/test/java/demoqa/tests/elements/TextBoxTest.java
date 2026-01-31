90d30c26053ab962c981295833d570bd01f3c674
Aleksandr Zhitenyov <aleksandr.zhitenyov@altenar.com>
2026-01-31 13:06:22 +0300

    add text-box test

---
+++ src/test/java/demoqa/tests/elements/TextBoxTest.java
@@ -0,0 +1,32 @@
+package demoqa.tests.elements;
+
+import demoqa.tests.TestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxTest extends TestBase {
+
+    @DisplayName("Successful submit form on demoqa/text-box")
+    @Tags({@Tag("UI_TEST"), @Tag("MAJOR")})
+    @Test
+    void shouldSubmitForm() {
+        open("/text-box");
+
+        $x("//h1").should(visible);
+        $x("//*[@id='userName']").setValue("userNameText");
+        $x("//*[@id='userEmail']").setValue("name@example.com");
+        $x("//*[@id='currentAddress']").setValue("Moscow city");
+        $x("//*[@id='permanentAddress']").setValue("St. Petersburg city");
+        $x("//*[@id='submit']").click();
+
+        $x("//*[@id='output']//*[@id='name']").shouldHave(text("usernametext")); // Contains case-insensitive text
+        $x("//*[@id='output']").find("#email").shouldHave(exactText("email:name@example.com")); // Case-insensitive exact match
+        $x("//*[@id='output']").$x(".//*[@id='currentAddress']").shouldHave(textCaseSensitive("Moscow city")); // Contains case-sensitive text
+        $("#output").$x(".//*[@id='permanentAddress']").shouldHave(exactTextCaseSensitive("Permananet Address :St. Petersburg city")); // Case-sensitive exact match
+    }
+}
