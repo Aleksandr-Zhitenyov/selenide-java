866b1357bafece6cb49adb9772423cf0448ba061
Aleksandr Zhitenyov <aleksandr.zhitenyov@altenar.com>
2026-02-11 22:51:33 +0300

    add allure

--- src/test/java/demoqa/tests/TestBase.java
+++ src/test/java/demoqa/tests/TestBase.java
@@ -1,6 +1,8 @@
 package demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -9,5 +11,7 @@ public class TestBase {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1500x900";
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 }
